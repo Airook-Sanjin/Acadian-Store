28c5180c23e1fcb980467fabff3f4f98c4b3d560
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-04-14 17:13:26 -0400

    justaddedBasicHomepage

--- Auth/Login.py
+++ Auth/Login.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from globals import Flask, secrets, redirect, url_for, Connecttodb, text ,render_template,request
 from Auth.Register import register_who
+from User.Admin import admin
 
 login_bp = Blueprint('login_bp',__name__,url_prefix='/auth',template_folder='templates',static_folder='static',static_url_path='/static')
+login_bp.register_blueprint(admin)
 conn = Connecttodb()
 
 @login_bp.route('/Login',methods=["GET"])
@@ -12,22 +14,28 @@ def Login():
 
 @login_bp.route('/Login',methods=["POST"])
 def Signin():
-    
+    # Creates dictionary view of all users,customer, vendor and admin
     Allusers = conn.execute(text('Select * from users')).mappings().fetchall()
     Allcustomers = conn.execute(text('Select * from customer')).mappings().fetchall()
     AllAdmins = conn.execute(text('Select * from admin')).mappings().fetchall()
+    AllVendors = conn.execute(text('Select * from vendor')).mappings().fetchall()
     print(Allusers)
     email = request.form.get('Email')
+    
     try:
         if any(customer['email'] == email for customer in Allcustomers ):
             print('INTO EMAIL')
-            return "CUSTOMER"
+            return redirect(url_for('login_bp.admin.AdminHomePage'))
         elif any(admin['email'] == email for admin in AllAdmins ):
+            login_bp.add_url_rule('/admin/Home', view_func=admin.AdminHomePage, endpoint='Admin')
             print('INTO Admin')
-            return "Admin"
+            return redirect(url_for('login_bp.Admin'))
+        elif any(vendor['email'] == email for vendor in AllVendors ):
+            print('INTO VENDOR')
+            return 'Vendor'
         else:
-            print('INTO Vendor')
-            return"Vendor"
+            print('Not a User')
+            return"Not A USER"
         
     except Exception as e:
         print(e)
--- dbconnect.py
+++ dbconnect.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, text, update
+#  function that connects to database
 def Connecttodb():
+    
     conn_str = "mysql://root:cset155@localhost/acadiandb" # connects to DataBase
     engine = create_engine(conn_str, echo=True)
     return engine.connect()
