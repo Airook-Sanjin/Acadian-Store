e58ff4dd238b00a3b14f57fa30f5996c12cac884
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-04-19 00:29:27 -0400

    Updated all queries

--- User/user_util/cart/cart.py
+++ User/user_util/cart/cart.py
@@ -3,8 +3,14 @@ from globals import Blueprint, render_template, request,g,session,redirect,url_f
 cart_bp = Blueprint('cart_bp', __name__, url_prefix='/cart', template_folder='templates')
 
 conn = Connecttodb()
+
 @cart_bp.before_request # Before each request it will look for the values below
 def load_user():
+    try:
+        conn.execute(text('SELECT 1')).fetchone()
+    except Exception:
+        print("Reconnecting to DB....")
+        conn=Connecttodb()
         
     if "User" in session:
         g.User = session["User"]
@@ -16,17 +22,21 @@ def UserCart(username):
     try:
         CartList = conn.execute(text(
             """
-                SELECT ca.ITEM_ID AS itemid, ca.title AS title, ca.size AS size, ca.color AS color, p.description as description 
+                SELECT ca.ITEM_ID AS itemid, ca.title AS title, ca.size AS size, ca.color AS color, p.description as description,p.price as price
                 FROM cart AS ca LEFT JOIN CUSTOMER AS cu ON ca.CID = cu.CID LEFT JOIN product as p on ca.PID = p.PID
                 WHERE ca.CID = :ID """),{'ID': g.User['ID']}).mappings().fetchall()
         print (CartList)
-
+        total = 0
+        for item in CartList:
+            total+=float(item['price'])
+            
+        print(total)
         if not g.User: #* Handles if signed in or not
             return redirect(url_for('login_bp.Login'))
-        return render_template('Cart.html',username=username,CartList = CartList)
+        return render_template('Cart.html',username=username,CartList = CartList,total =total)
     except Exception as e:
         print(f'ERROR: {e}')
-        return render_template('Cart.html',username=username,CartList = CartList)
+        return render_template('Cart.html',username=username,CartList = CartList,total = 0)
     
 @cart_bp.route('/<username>', methods=['POST'])
 def RemoveFromCart(username):
@@ -42,11 +52,15 @@ def RemoveFromCart(username):
         
         CartList = conn.execute(text(
             """
-                SELECT ca.ITEM_ID AS itemid, ca.title AS title, ca.size AS size, ca.color AS color, p.description as description 
+                SELECT ca.ITEM_ID AS itemid, ca.title AS title, ca.size AS size, ca.color AS color,p.description as description,p.price as price
                 FROM cart AS ca LEFT JOIN CUSTOMER AS cu ON ca.CID = cu.CID LEFT JOIN product as p on ca.PID = p.PID
                 WHERE ca.CID = :ID """),{'ID': g.User['ID']}).mappings().fetchall() #* Updated Cart List
         
-        return render_template('Cart.html',username=username,CartList = CartList)
+        total = 0
+        for item in CartList:
+            total+=float(item['price'])
+            
+        return render_template('Cart.html',username=username,CartList = CartList,total=total)
     except Exception as e:
         print(f'ERROR: {e}')
         return render_template('Cart.html',username=username,CartList = CartList)
@@ -54,9 +68,14 @@ def RemoveFromCart(username):
 @cart_bp.route('/add',methods=['POST'])
 def addToCart():
     try:
+        Allproducts = conn.execute(text(
+        """SELECT p.PID as PID,p.title as title, p.price as price,p.description as description,inv.amount as amount,p.warranty as warranty,p.discount as discount,p.availability as availability,p.image_url as image FROM product as p
+           LEFT JOIN product_images as pi on p.PID = pi.PID
+           LEFT JOIN product_inventory as inv on pi.PID = inv.PID""")).mappings().fetchall() #! Needed to then get the PID
+        
         EMAIL = g.User['Email']
         ID = g.User['ID']
-        PID = request.form.get('PID')
+        PID = int(request.form.get('PID'))
         conn.execute(text(
             """
             INSERT INTO cart 
@@ -69,17 +88,17 @@ def addToCart():
         if g.User['Role']=='customer': #* checks role
             
             print('INTO Customer')
-            return redirect(url_for('customer_bp.CustomerHomePage', username=g.User['Name'])) # * Takes you to Customer page
+            return redirect(url_for('customer_bp.CustomerHomePage',Allproducts = Allproducts, username=g.User['Name'])) # * Takes you to Customer page
         
         elif g.User['Role']=='admin': #* checks role
             print('INTO Admin')
 
-            return redirect(url_for('admin.AdminHomePage', username=g.User['Name'])) # * Takes you to admin page
+            return redirect(url_for('admin.AdminHomePage',Allproducts = Allproducts, username=g.User['Name'])) # * Takes you to admin page
 
         elif g.User['Role']=='vendor': #* checks role
 
             print('INTO VENDOR')
-            return redirect(url_for('vendor_bp.VendorHomePage', username=g.User['Name'])) # * Takes you to vendor page
+            return redirect(url_for('vendor_bp.VendorHomePage',Allproducts = Allproducts, username=g.User['Name'])) # * Takes you to vendor page
 
     except Exception as e:
         print(f'ERROR: {e}')
