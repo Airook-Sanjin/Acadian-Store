82b1049972c34db7dc39adbf5a486f3394f1acbc
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-04-28 17:29:18 -0400

    Fixed CartUpdates after Placing Order

--- User/user_util/PlaceOrder/PlaceOrder.py
+++ User/user_util/PlaceOrder/PlaceOrder.py
@@ -25,16 +25,21 @@ def placeOrder():
         
         CurTime = datetime.now()
         print(str(CurTime)[0:19])
-        
-        listofitemid = tuple([item['itemid'] for item in CartList])
+        # * Get ItemIDs without an ORDER_ID
+        ItemIDTable = conn.execute(text(
+            """
+                SELECT ca.ITEM_ID AS itemid,ca.ORDER_ID as OrderID
+                FROM cart AS ca LEFT JOIN CUSTOMER AS cu ON ca.CID = cu.CID LEFT JOIN product as p on ca.PID = p.PID
+                WHERE ca.CID = :ID AND ca.ORDER_ID is Null """),{'ID': g.User['ID']}).mappings().fetchall()
+        listofitemid = tuple([item['itemid'] for item in ItemIDTable])
         print(listofitemid)
         # * Get Count of cart items
         CartQuantity = conn.execute(text(
             """
                 SELECT sum(ca.quantity) as CartQuantity, ca.ORDER_ID as OrderID 
                 FROM cart AS ca LEFT JOIN CUSTOMER AS cu ON ca.CID = cu.CID LEFT JOIN product as p on ca.PID = p.PID
-                WHERE ca.CID = :ID
-                Group By ca.ORDER_ID;"""),{'ID': g.User['ID']}).mappings().fetchone()
+                WHERE ca.CID = :ID AND ca.ORDER_ID is Null
+                Group By ca.ORDER_ID"""),{'ID': g.User['ID']}).mappings().fetchone()
     
         print(CartQuantity['CartQuantity'])
         print(CartQuantity) #!DEBUG
@@ -45,17 +50,19 @@ def placeOrder():
 	                (date,total,amount)
                 VALUES
 	                (now(),:total, :quantity);"""),{'total':total,'quantity':int(CartQuantity['CartQuantity'])})
+        conn.commit()
+        # * Gets Recent OrderID
+        RecentOrder=conn.execute(text("""
+                SELECT max(ORDER_ID) as recent from orders""")).mappings().fetchone()
         
-        Order=conn.execute(text("""
-                Select ORDER_ID from order"""))
-        
-        # conn.commit()
+        conn.commit()
         # * Updates Cart items
         conn.execute(text("""
                 Update cart
-                set ORDER_ID = 7
-                Where ITEM_ID in :itemids;"""),{'itemids':listofitemid})
-        
+                set ORDER_ID = :recent
+                Where CID = :cid AND (ORDER_ID IS NULL OR ORDER_ID = 0) AND ITEM_ID in :itemids;"""),
+                {'cid':g.User['ID'],'itemids':listofitemid,'recent':RecentOrder['recent']})
+        conn.commit()
         
         return render_template('OrderPlaced.html')
     except Exception as e:
