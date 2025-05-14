8995558e48f05faf23aa2521e962ed2dacf2d019
Darlin <darli2819@gmail.com>
2025-05-13 22:12:40 -0400

    added hashing| admin product validation

--- User/Admin/Admin.py
+++ User/Admin/Admin.py
@@ -1,5 +1,5 @@
 
-from globals import Blueprint,render_template,g,session,Connecttodb,text,request,checkAndUpdateOrder
+from globals import Blueprint, render_template, request,g,session,Connecttodb,text,checkAndUpdateOrder,CheckOrderDelivered
 from datetime import datetime
 from User.chat import chat_bp
 from threading import Timer
@@ -538,6 +538,74 @@ def updateClaimStatus():
         print("Error updating status:", e)
 
     return redirect(url_for('admin_bp.claimHistory'))
+
+
+@admin_bp.route('/RecievedOrder',methods=["POST"])
+def GetProfileOrderHistory():
+    conn.commit()
+    try:
+        if not g.User: #* Handles if signed in or not
+            return redirect(url_for('login_bp.Login'))
+        checkAndUpdateOrder()
+        CheckOrderDelivered()
+        return redirect(url_for('admin_bp.AdminRecievedOrders'))
+    except Exception as e:
+        print(f"Error POST: {e}")
+        return redirect(url_for('admin_bp.AdminRecievedOrders'))
+
+@admin_bp.route('/RecievedHistory',methods=["GET"])
+def AdminRecievedOrders():
+    
+    try:
+        if not g.User: #* Handles if signed in or not
+            return redirect(url_for('login_bp.Login'))
+        checkAndUpdateOrder()
+        CheckOrderDelivered()
+        PlacedOrders= conn.execute(text("""
+            select o.ORDER_ID as OID,o.total as total, o.status as OrderStatus,o.DateShipped as DateShipped,date(date_add(o.DateShipped, interval 5 DAY)) as DeliveryDate, ca.ITEM_ID as ItemID, p.title as Itemtitle, ca.color as ItemColor,ca.quantity as ItemQuantity,
+            CASE
+            	WHEN p.discount IS NULL OR p.discount_date > curdate() THEN p.price * ca.quantity
+            	WHEN p.discount IS NOT NULL OR p.discount_date < curdate() THEN (p.price - (p.price * p.discount)) * ca.quantity 
+            END AS ItemPrice, ca.ItemStatus AS ItemStatus, ca.CID as CustID,Date(ca.DateShipped) as DateShipped, v.email as CustEmail FROM cart AS ca 
+            LEFT JOIN orders as o on ca.ORDER_ID = o.ORDER_ID 
+            LEFT JOIN product as p on ca.PID =p.PID 
+            LEFT JOIN admin as v on p.AID = v.AID
+            LEFT JOIN users as u on v.email = u.email
+            WHERE v.AID = :ID and o.ORDER_ID is Not Null
+            Order By o.ORDER_ID; """),{'ID': g.User['ID']}).mappings().fetchall()
+        GroupedOrders={} #* Dictionary to keep all the values of PlacedOrder
+        for row in PlacedOrders:
+            OrderId = row['OID'] #* Extracts order ID
+            if OrderId not in GroupedOrders:
+                GroupedOrders[OrderId]={
+                    "OrderId":OrderId,
+                    "OrderStatus":row['OrderStatus'],
+                    "OrderDateShipped":row['DateShipped'],
+                    "OrderTotal":row['total'],
+                    "Items":[]
+                }
+            # * 
+            GroupedOrders[OrderId]["Items"].append({
+                "ItemID":row['ItemID'],
+                "ItemTitle":row['Itemtitle'],
+                "ItemColor":row['ItemColor'],
+                "ItemQuantity":row['ItemQuantity'],
+                "ItemPrice":row['ItemPrice'],
+                "ItemStatus":row['ItemStatus'],
+                "DateShipped":row['DateShipped'],
+                "ItemDeliveryDate":row['DeliveryDate']
+                
+                })
+        GroupedOrdersList = list(GroupedOrders.values())
+        print(f"Grouped Orders: {GroupedOrdersList}")
+            
+        
+        return render_template('AdminRecievedOrders.html', GroupedOrders=GroupedOrdersList)
+        
+        
+    except Exception as e:
+        print(f"Error GET: {e}")
+        return render_template('AdminRecievedOrders.html',GroupedOrders=[])
 
 
 
